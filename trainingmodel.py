7754aa657dd377b8f6cfb4762f2eeb1346733274
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-03 10:05:25 +0300

    Changed the window width on the training file

--- trainingmodel.py
+++ trainingmodel.py
@@ -15,8 +15,8 @@ pygame.init()
 pygame.display.set_caption("FlappyBirdAI (NEAT)")
 
 # Constants
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 FONT = pygame.font.SysFont("comicsans", 50)
